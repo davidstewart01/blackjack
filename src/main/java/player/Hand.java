78509a268ef2cca9406fccea1a32068679fc5ddb
David Stewart <david.stewart01@gmail.com>
2017-08-30 18:51:35 +0100

    Ading Hand class

---
+++ src/main/java/player/Hand.java
@@ -0,0 +1,104 @@
+package player;
+
+import card.Card;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Class that represents the hand of cards that a player can bet on.
+ *
+ * @author Paul Anderson
+ */
+public class Hand extends LinkedList<Card> {
+
+  //----------------------------------------
+  // property: Bet
+  //----------------------------------------
+  private double mBet = 0;
+
+  /**
+   * @return
+   *   The bet wagered on the Hand.
+   */
+  public double getBet() {
+    return mBet;
+  }
+
+  /**
+   * @param pBet
+   *   The bet wagered on the Hand.
+   */
+  public void setBet(double pBet) {
+    mBet = pBet;
+  }
+
+  //----------------------------------------
+  // property: hasDoubledDown
+  //----------------------------------------
+  private boolean mHasDoubledDown = false;
+
+  /**
+   * @return
+   *   Whether or not the player has made a Double Down play.
+   */
+  public boolean hasDoubledDown(){
+    return mHasDoubledDown;
+  }
+
+  /**
+   * @param pHasDoubledDown
+   */
+  public void setHasDoubledDown(boolean pHasDoubledDown) {
+    mHasDoubledDown = pHasDoubledDown;
+  }
+
+  //----------------------------------------
+  // Variables
+  //----------------------------------------
+  private boolean isSplittable = false;
+
+
+  /**
+   * Double down the wagered bet on the Hand in play.  This can only be called on the first play on the hand,
+   * when it has only 2 cards.
+   */
+  public void doubleDown(){
+    if(!hasDoubledDown() && numberOfCardsInHand() == 2){
+      setHasDoubledDown(true);
+      setBet(getBet() * 2);
+    }
+  }
+
+  /**
+   * @return
+   *   Whether or not the player can split the current hand
+   */
+  public boolean canSplitHand(){
+    if((numberOfCardsInHand() == 2) && (get(0).getValue() == get(1).getValue())){
+      isSplittable = true;
+    }
+    return isSplittable;
+  }
+
+  /**
+   * Adds a Card to the current players Hand.
+   *
+   * @param pCard
+   *   the Card to add to the Hand
+   */
+  public void addCard(Card pCard){
+    add(pCard);
+  }
+
+  /**
+   * Returns the number of Cards in the Hand.
+   *
+   * @return
+   *   The number of Cards in the current Hand.
+   */
+  public int numberOfCardsInHand(){
+    return size();
+  }
+
+}
